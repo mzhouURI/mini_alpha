74595583bd839e02c0eccb3fbc60a7be59af1e70
mzhouURI <mzhou@uri.edu>
2025-06-27 19:54:05 +0000

    added power monitor and pressure

--- mini_alpha_bringup/launch/bringup_sensors.launch.py
+++ mini_alpha_bringup/launch/bringup_sensors.launch.py
@@ -8,19 +8,51 @@ def generate_launch_description():
     robot_name = 'mini_alpha'
     robot_bringup = robot_name + '_bringup'
 
-    parameters_file = os.path.join(get_package_share_directory(robot_bringup), 'config', 'xsens.yaml')
+    xsens_parameters_file = os.path.join(
+        get_package_share_directory(robot_bringup), 
+        'config', 
+        'xsens.yaml')
+
+    presure_parameters = os.path.join(
+        get_package_share_directory(robot_bringup),
+        'config',
+        'bluerobotics_bar30.yaml'
+    )
+
+    power_moinitor_param = os.path.join(
+        get_package_share_directory(robot_bringup),
+        'config',
+        'power_monitor.yaml'
+    )
 
     return LaunchDescription([
+        # Node(
+        #     package='xsens_mti_ros2_driver',
+        #     namespace=robot_name,
+        #     executable='xsens_mti_node',
+        #     name='xsens_mti_node',
+        #     prefix=['stdbuf -o L'],
+        #     output="screen",
+        #     parameters=[xsens_parameters_file],
+        #     emulate_tty=True
+        # ),
         Node(
-            package='xsens_mti_ros2_driver',
+            package='bluerobotics_pressure',
+            executable='bluerobotics_pressure_node',
+            name='bluerobotics_pressure_node',
             namespace=robot_name,
-            executable='xsens_mti_node',
-            name='xsens_mti_node',
-            prefix=['stdbuf -o L'],
-            output="screen",
-            parameters=[parameters_file],
-            emulate_tty=True
+            output='screen',
+            parameters=[presure_parameters]        
+        ),
 
+        Node(
+            package='power_monitor',
+            executable='power_monitor_node',
+            name='power_monitor_node',
+            namespace=robot_name,
+            output='screen',
+            parameters=[power_moinitor_param]        
         )
+
        
     ])

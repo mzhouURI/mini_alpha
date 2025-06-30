e6e8521616d2dee57d813379248a328baad2b53b
mzhouURI <mzhou@uri.edu>
2025-06-30 23:02:49 +0000

    updated static tf

--- mini_alpha_bringup/launch/include/description.launch.py
+++ mini_alpha_bringup/launch/include/description.launch.py
@@ -41,7 +41,7 @@ def generate_launch_description():
             package='tf2_ros',
             executable='static_transform_publisher',
             name='world2ned',
-            arguments = ["0.0", "0.0", "0.0", "1.571", "0.0", "3.1415", robot_name+'/world', robot_name+'/world_ned']    
+            arguments = ["0.0", "0.0", "0.0", "1.5707963", "0.0", "3.1415926", robot_name+'/world', robot_name+'/world_ned']    
         ),
 
         Node(
